a2829d5e339abba9af9bdfc1d323c0b6f8d6b792
jeonghyunseok <jhs92043@gmail.com>
2023-09-08 08:24:58 +0900

    feat. 로컬 작업 시작

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,10 +1,7 @@
-var store = [
-  {
-    title: "[포스팅 예시] 이곳에 제목을 입력하세요",
-    excerpt: "🦥 본문   본문은 여기에 …  ",
-    categories: ["Categories1"],
-    tags: ["tag1", "tag2"],
-    url: "/categories1/post-name-here/",
-    teaser: null,
-  },
-];
+var store = [{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Categories1"],
+        "tags": ["tag1","tag2"],
+        "url": "/categories1/post-name-here/",
+        "teaser": null
+      }]
